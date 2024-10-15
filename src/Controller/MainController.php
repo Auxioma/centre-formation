e31fd8b11c5b0f543553d50c1a129da0862dace7
Guillaume DEVAUX (Auxioma) <auxioma.g@gmail.com>
2024-10-15 09:04:56 +0200

    apache

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
+    public function __construct(
+        private readonly CategoryRepository $categoryRepository
+    ){}
     #[Route('/', name: 'app_main')]
     public function index(): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        return $this->render('main/index.html.twig');
     }
 }
--- src/Controller/Navigation/NavBarController.php
+++ src/Controller/Navigation/NavBarController.php
@@ -2,13 +2,20 @@
 
 namespace App\Controller\Navigation;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class NavBarController extends AbstractController 
 {
+    public function __construct(
+        private CategoryRepository $categoryRepository
+    ){}
+
     public function navBar()
     {
-        return $this->render('_partials/navigation/header.html.twig');
+        return $this->render('_partials/navigation/header.html.twig', [
+            'categories' => $this->categoryRepository->findBy(['parent' => null])
+        ]);
     }
 
     public function navBarMobile()
