55f11fab3339859ebf04870c50b0b443af94a2f0
Guillaume DEVAUX (Auxioma) <auxioma.g@gmail.com>
2024-10-20 17:19:29 +0200

    maintenance

--- src/EventListener/MaintenanceListener.php
+++ src/EventListener/MaintenanceListener.php
@@ -6,39 +6,30 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Twig\Environment;
 
-class MaintenanceListener
-{
+class MaintenanceListener{
     private $maintenance;
     private $twig;
 
-    public function __construct($maintenance, Environment $twig)
-    {
+    public function __construct($maintenance, Environment $twig){
         $this->maintenance = $maintenance;
         $this->twig = $twig;
     }
 
-    public function onKernelRequest(RequestEvent $event)
-    {
-        // Si le fichier .maintenance n'existe pas, on sort
-        if (!file_exists($this->maintenance)) {
+    public function onKernelRequest(RequestEvent $event){
+        // On vérifie si le fichier .maintenance n'existe pas
+        if(!file_exists($this->maintenance)){
             return;
         }
 
-        // Vérifier la présence du paramètre dans l'URL
-        $request = $event->getRequest();
-        if ($request->query->get('bypass') === '1') {
-            return;
-        }
-
-        // Si le paramètre n'est pas présent, afficher la page de maintenance
+        // Le fichier existe
+        // On définit la réponse
         $event->setResponse(
             new Response(
                 $this->twig->render('maintenance/maintenance.html.twig'),
                 Response::HTTP_SERVICE_UNAVAILABLE
             )
         );
-        
-        // Stoppe le traitement des événements
+        // On stoppe le traitement des évènements
         $event->stopPropagation();
     }
 }
