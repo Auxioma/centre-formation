d9eba20bf02a0ae8830df505a705204906a3d024
Auxioma <auxioma@yandex.fr>
2025-07-22 10:14:41 +0200

    mise  a jours des routes

--- src/Controller/Admin/AppsController.php
+++ src/Controller/Admin/AppsController.php
@@ -10,56 +10,56 @@ class AppsController extends AbstractController
 {
     public function chat(): Response
     {
-        return $this->render('apps/chat.html.twig');
+        return $this->render('admin/apps/chat.html.twig');
     }
 
     public function mailbox(): Response
     {
-        return $this->render('apps/mailbox.html.twig');
+        return $this->render('admin/apps/mailbox.html.twig');
     }
 
     public function todo_list(): Response
     {
-        return $this->render('apps/todolist.html.twig');
+        return $this->render('admin/apps/todolist.html.twig');
     }
 
     public function notes(): Response
     {
-        return $this->render('apps/notes.html.twig');
+        return $this->render('admin/apps/notes.html.twig');
     }
 
     public function scrumboard(): Response
     {
-        return $this->render('apps/scrumboard.html.twig');
+        return $this->render('admin/apps/scrumboard.html.twig');
     }
 
     public function contacts(): Response
     {
-        return $this->render('apps/contacts.html.twig');
+        return $this->render('admin/apps/contacts.html.twig');
     }
 
     public function invoice_list(): Response
     {
-        return $this->render('apps/invoice/list.html.twig');
+        return $this->render('admin/apps/invoice/list.html.twig');
     }
 
     public function invoice_add(): Response
     {
-        return $this->render('apps/invoice/add.html.twig');
+        return $this->render('admin/apps/invoice/add.html.twig');
     }
 
     public function invoice_preview(): Response
     {
-        return $this->render('apps/invoice/preview.html.twig');
+        return $this->render('admin/apps/invoice/preview.html.twig');
     }
 
     public function invoice_edit(): Response
     {
-        return $this->render('apps/invoice/edit.html.twig');
+        return $this->render('admin/apps/invoice/edit.html.twig');
     }
 
     public function calendar(): Response
     {
-        return $this->render('apps/calendar.html.twig');
+        return $this->render('admin/apps/calendar.html.twig');
     }
 }
